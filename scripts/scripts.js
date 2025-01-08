d2af55db8e3419c0c8c03a082a28604dfdadecad
Surajit Singha Sisir <sci.surajit@gmail.com>
2025-01-08 21:58:52 +0600

    modal box added
    Signed-off-by: Surajit Singha Sisir <sci.surajit@gmail.com>

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -91,6 +91,8 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   updatePrice();
+  modalBox();
+  cartBox();
 });
 
 // UPPDATING PRICE
@@ -106,3 +108,89 @@ function updatePrice() {
   });
 }
 
+function cart(item) {
+  if (item.dataset.disabled === "true") return;
+
+  item.dataset.disabled = "true";
+
+  function anim() {
+    const icon = item.querySelector(".m-plus-circle");
+    const p = item.querySelector("p");
+    if (icon && p) {
+      icon.classList.add("hide");
+      p.classList.add("hide");
+      const animImg = document.createElement("img");
+      animImg.src = "../assets/cart-added.gif";
+      const div = item.querySelector("div");
+      div.appendChild(animImg);
+      if (!div.classList.contains("cart-animation")) {
+        div.classList.toggle("cart-animation");
+      }
+
+      setTimeout(() => {
+        icon.classList.remove("hide");
+        p.classList.remove("hide");
+        item.dataset.disabled = "false";
+
+        const anim = item.querySelector(".cart-animation img");
+        if (anim) {
+          anim.closest("div").classList.toggle("cart-animation");
+          anim.remove();
+        }
+      }, 3000);
+    }
+  }
+  anim();
+}
+function modalBox() {
+  const modalOverlay = document.getElementById("modalOverlay");
+  const modal = modalOverlay.querySelector(".modal");
+
+  const openModal = document.getElementById("modalBox-1");
+
+  // TARGET COLLECTION
+  modalOverlay.style.backgroundColor = modalOverlay.getAttribute("target-bg");
+  modal.style.backgroundColor = modal.getAttribute("modal-bg");
+  modal.style.color = modal.getAttribute("modal-color");
+  modal.style.width = modal.getAttribute("modal-width");
+  modal.style.height = modal.getAttribute("modal-height");
+  const scroller = modal.getAttribute("scroller");
+  console.log(scroller);
+
+  openModal.addEventListener("click", () => {
+    modalOverlay.style.display = "flex";
+    modal.classList.remove("modal-out");
+    modal.classList.add("modal-in");
+
+    setTimeout(() => {
+      if (scroller == "true") {
+        document.body.style = "overflow: hidden !important;";
+      }
+    }, 2000);
+  });
+
+  const closeModal = document.getElementById("closeModalBox");
+  closeModal.addEventListener("click", () => {
+    modal.classList.remove("modal-in");
+    modal.classList.add("modal-out");
+    // After the animation ends, hide the modal
+    modal.addEventListener(
+      "animationend",
+      () => {
+        modalOverlay.style.display = "none";
+        modal.classList.remove("modal-out");
+
+        if (scroller == "true") {
+          document.body.style = "overflow: auto !important;";
+        }
+      },
+      { once: true }
+    );
+  });
+}
+
+
+function cartBox() {
+  const cartOverlay = document.getElementById("cartOverlay");
+  const cart = cartOverlay.querySelector(".cart");
+}
